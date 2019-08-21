16bf26229dbe31d50b994885fb92d2100a3b32a7
femiokunowo <50460156+femiokunowo@users.noreply.github.com>
2019-08-20 20:37:16 -0700

    Initial commit for Card Replacement Component

---
+++ force-app/main/default/aura/CardReplacementInit/cardReplacementInitCmpController.js
@@ -0,0 +1,14 @@
+({
+	
+    init : function(component, event, helper) {
+		
+        //TODO: other business logic can be placed here before navigating to the servicing workflow
+        
+        
+        //var pageReference = component.find("navigationServiceCmp").get("v.pageReference");
+        //console.log("Page Reference: " + pageReference);
+        //component.find("navigationServiceCmp").navigationHandler(); 
+             
+       
+	}   
+})
